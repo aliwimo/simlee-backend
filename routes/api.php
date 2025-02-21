fbd4053f772055881b4fe283b1399b5fd91a87fa
Mohamed Aliwi <aliwimo@gmail.com>
2025-02-21 22:22:32 +0300

    Add API routes for teams with index and showBySlug methods

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\API\V1\Team\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/v1')->group(function () {
+
+    /** teams routes */
+    Route::prefix('/teams')->group(callback: function () {
+        Route::get('/', [TeamController::class, 'index']);
+        Route::get('{slug}/show', [TeamController::class, 'showBySlug']);
+    });
+
 });
