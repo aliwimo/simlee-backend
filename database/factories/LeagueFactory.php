7178ed35f471655968b48886b697b2bc330903b6
Mohamed Aliwi <aliwimo@gmail.com>
2025-02-27 17:16:30 +0300

    Add LeagueFactory for generating league model instances

---
+++ database/factories/LeagueFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\League>
+ */
+class LeagueFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'season' => fake()->numberBetween(int1: 2000, int2: 2100),
+            'teams_number' => fake()->numberBetween(int1: 4, int2: 20),
+        ];
+    }
+}
